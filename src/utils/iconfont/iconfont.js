2398596019052ae0c0d1b5abd5c7b39dabc4cdbe
zx <zx>
2021-03-30 17:25:23 +0800

    123

--- src/utils/iconfont/iconfont.js
+++ src/utils/iconfont/iconfont.js
@@ -1,7 +1,7 @@
 import { createFromIconfontCN } from '@ant-design/icons'
 
 const IconFont = createFromIconfontCN({
-  scriptUrl: 'http://at.alicdn.com/t/font_2222373_3lbunot2ppl.js'
+  scriptUrl: 'https://at.alicdn.com/t/font_2222373_3lbunot2ppl.js'
 })
 
 export default IconFont

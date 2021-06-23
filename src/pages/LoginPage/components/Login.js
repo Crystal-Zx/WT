2d5d089c3f02dc9bccf60ead2dc9dc4af200330f
zx <zhouxi_aimee@163.com>
2021-06-23 17:43:26 +0800

    [优化]注释掉Login页面无用代码

--- src/pages/LoginPage/components/Login.js
+++ src/pages/LoginPage/components/Login.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import { Form, Input, Button, Radio } from 'antd'
-import LineTabs from '../../../components/LineTabs/LineTabs'
+// import LineTabs from '../../../components/LineTabs/LineTabs'
 import IconFont from '../../../utils/iconfont/iconfont'
 import user from '../../../services/user'
 import { getQueryString, popMessage } from '../../../utils/utilFunc'
@@ -9,7 +9,7 @@ const Login = (props) => {
   const { history } = props
 
   const [loading, setLoading] = useState(false)
-  const [activeKey, setActiveKey] = useState('0')
+  // const [activeKey, setActiveKey] = useState('0')
 
   const onFinishByOA = (values) => {
     // console.log("===onFinishByOA values:", values)
@@ -27,19 +27,19 @@ const Login = (props) => {
       setLoading(false)
     })
   }
-  const onFinishByMT4 = (values) => {
-    setLoading(true)
-    user.login(values, 'mt4').then(res => {
-      popMessage({ type: 'success', msg: '登录成功！' })
-      setTimeout(() => {  // 加一个延时，否则message来不及显示
-        // window.location.href = "./"
-        history.push("/index")
-      }, 1000);
-    }).catch(err => {
-      popMessage({ type: 'error', msg: err.msg || `${err}` })
-      setLoading(false)
-    })
-  }
+  // const onFinishByMT4 = (values) => {
+  //   setLoading(true)
+  //   user.login(values, 'mt4').then(res => {
+  //     popMessage({ type: 'success', msg: '登录成功！' })
+  //     setTimeout(() => {  // 加一个延时，否则message来不及显示
+  //       // window.location.href = "./"
+  //       history.push("/index")
+  //     }, 1000);
+  //   }).catch(err => {
+  //     popMessage({ type: 'error', msg: err.msg || `${err}` })
+  //     setLoading(false)
+  //   })
+  // }
   const autoLogin = () => {
     const token = getQueryString('t')
     if(token) {
@@ -55,114 +55,114 @@ const Login = (props) => {
       })
     }
   }
-  const onChangeAcc = (acc) => {
-    setActiveKey(acc)
-  }
-  const onChangeType = (e) => {
-    sessionStorage.setItem("wt_selectType", e.target.value)
-  }
-  const getFormJSX = (currKey) => {
-    switch(currKey) {
-      case '0':
-        return (
-          <Form
-            name="wt-login-oa"
-            className="wt-login"
-            layout="vertical"
-            initialValues={{ remember: true }}
-            onFinish={onFinishByOA}
-          >
-            <Form.Item 
-              name="username"
-              label="ALPHAZONE账号"
-              rules={[{ required: true, message: '请输入ALPHAZONE账号'}]}
-            >
-              <Input 
-                prefix={<IconFont type="iconUser" className="wt-icon" />}
-                placeholder="请输入ALPHAZONE账号"
-              />
-            </Form.Item>
-            <Form.Item 
-              name="password"
-              label="密码"
-              rules={[{ required: true, message: '请输入密码'}]}
-            >
-              <Input.Password 
-                prefix={<IconFont type="iconLock" className="wt-icon" />}
-                placeholder="请输入密码"
-              />
-            </Form.Item>
-            <Form.Item >
-              没有ALPHAZONE账号？&nbsp;&nbsp;
-              <a href="https://www.alphazone.com.cn/register.html">创建一个&nbsp;&gt;&gt;</a>
-            </Form.Item>
-            <Form.Item noStyle>
-              <Button 
-                type="primary"
-                htmlType="submit"
-                className="wt-btn-ok lg-btn-submit"
-                loading={loading}
-              >
-                登录
-              </Button>
-            </Form.Item>
-          </Form>
-        )
-      case '1':
-        return (
-          <Form
-            name="wt-login-mt4"
-            className="wt-login"
-            layout="vertical"
-            initialValues={{ remember: true }}
-            onFinish={onFinishByMT4}
-          >
-            <Form.Item 
-              name="login"
-              label="MT4账号"
-              rules={[{ required: true, message: '请输入MT4账号'}]}
-            >
-              <Input 
-                prefix={<IconFont type="iconUser" className="wt-icon" />}
-                placeholder="请输入MT4账号"
-              />
-            </Form.Item>
-            <Form.Item 
-              name="password"
-              label="密码"
-              rules={[{ required: true, message: '请输入密码'}]}
-            >
-              <Input.Password 
-                prefix={<IconFont type="iconLock" className="wt-icon" />}
-                placeholder="请输入密码"
-              />
-            </Form.Item>
-            <Form.Item
-              name="type"
-            >
-              <Radio.Group 
-                defaultValue={1}
-                onChange={onChangeType}
-              >
-                <Radio value={1}>模拟账户</Radio>
-                <Radio value={2}>实盘账户</Radio>
-              </Radio.Group>
-            </Form.Item>
-            <Form.Item noStyle>
-              <Button 
-                type="primary"
-                htmlType="submit"
-                className="lg-btn-submit"
-                loading={loading}
-              >
-                登录
-              </Button>
-            </Form.Item>
-          </Form>
-        )
-      default: return
-    }
-  }
+  // const onChangeAcc = (acc) => {
+  //   setActiveKey(acc)
+  // }
+  // const onChangeType = (e) => {
+  //   sessionStorage.setItem("wt_selectType", e.target.value)
+  // }
+  // const getFormJSX = (currKey) => {
+  //   switch(currKey) {
+  //     case '0':
+  //       return (
+  //         <Form
+  //           name="wt-login-oa"
+  //           className="wt-login"
+  //           layout="vertical"
+  //           initialValues={{ remember: true }}
+  //           onFinish={onFinishByOA}
+  //         >
+  //           <Form.Item 
+  //             name="username"
+  //             label="ALPHAZONE账号"
+  //             rules={[{ required: true, message: '请输入ALPHAZONE账号'}]}
+  //           >
+  //             <Input 
+  //               prefix={<IconFont type="iconUser" className="wt-icon" />}
+  //               placeholder="请输入ALPHAZONE账号"
+  //             />
+  //           </Form.Item>
+  //           <Form.Item 
+  //             name="password"
+  //             label="密码"
+  //             rules={[{ required: true, message: '请输入密码'}]}
+  //           >
+  //             <Input.Password 
+  //               prefix={<IconFont type="iconLock" className="wt-icon" />}
+  //               placeholder="请输入密码"
+  //             />
+  //           </Form.Item>
+  //           <Form.Item >
+  //             没有ALPHAZONE账号？&nbsp;&nbsp;
+  //             <a href="https://www.alphazone.com.cn/register.html">创建一个&nbsp;&gt;&gt;</a>
+  //           </Form.Item>
+  //           <Form.Item noStyle>
+  //             <Button 
+  //               type="primary"
+  //               htmlType="submit"
+  //               className="wt-btn-ok lg-btn-submit"
+  //               loading={loading}
+  //             >
+  //               登录
+  //             </Button>
+  //           </Form.Item>
+  //         </Form>
+  //       )
+  //     case '1':
+  //       return (
+  //         <Form
+  //           name="wt-login-mt4"
+  //           className="wt-login"
+  //           layout="vertical"
+  //           initialValues={{ remember: true }}
+  //           onFinish={onFinishByMT4}
+  //         >
+  //           <Form.Item 
+  //             name="login"
+  //             label="MT4账号"
+  //             rules={[{ required: true, message: '请输入MT4账号'}]}
+  //           >
+  //             <Input 
+  //               prefix={<IconFont type="iconUser" className="wt-icon" />}
+  //               placeholder="请输入MT4账号"
+  //             />
+  //           </Form.Item>
+  //           <Form.Item 
+  //             name="password"
+  //             label="密码"
+  //             rules={[{ required: true, message: '请输入密码'}]}
+  //           >
+  //             <Input.Password 
+  //               prefix={<IconFont type="iconLock" className="wt-icon" />}
+  //               placeholder="请输入密码"
+  //             />
+  //           </Form.Item>
+  //           <Form.Item
+  //             name="type"
+  //           >
+  //             <Radio.Group 
+  //               defaultValue={1}
+  //               onChange={onChangeType}
+  //             >
+  //               <Radio value={1}>模拟账户</Radio>
+  //               <Radio value={2}>实盘账户</Radio>
+  //             </Radio.Group>
+  //           </Form.Item>
+  //           <Form.Item noStyle>
+  //             <Button 
+  //               type="primary"
+  //               htmlType="submit"
+  //               className="lg-btn-submit"
+  //               loading={loading}
+  //             >
+  //               登录
+  //             </Button>
+  //           </Form.Item>
+  //         </Form>
+  //       )
+  //     default: return
+  //   }
+  // }
 
   useEffect(() => {
     autoLogin()
